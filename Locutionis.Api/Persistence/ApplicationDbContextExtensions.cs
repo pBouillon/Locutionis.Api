d952b7b7abc368851c67dac144ca0d6da35fa151
pBouillon <pro.pierre.bouillon@protonmail.com>
2023-01-26 10:33:24 +0100

    Add the antanaclase

--- Locutionis.Api/Persistence/ApplicationDbContextExtensions.cs
+++ Locutionis.Api/Persistence/ApplicationDbContextExtensions.cs
@@ -29,7 +29,7 @@ internal static class ApplicationDbContextExtensions
                 new()
                 {
                     DisplayName = "La culture générale",
-                    Url = new UriBuilder("https://www.laculturegenerale.com/alliteration-definition-exemples/").Uri)
+                    Url = new UriBuilder("https://www.laculturegenerale.com/alliteration-definition-exemples/").Uri,
                 }
             },
             Usages = new List<Usage>
@@ -51,6 +51,58 @@ internal static class ApplicationDbContextExtensions
                 }
             }
         },
+
+        // Antanaclase
+        new FigureOfSpeech
+        {
+            Name = "Antanaclase",
+            Description = """
+            On parle d'antanaclase lorsque l'on emploie deux fois un même mot mais 
+            avec un sens différent: on parle alors de polysémie (plusieurs sens). 
+            Si le mot n'est pas répété il est alors question d'antanaclase elliptique.
+            """,
+            Purpose = """
+            Il s'agit d'une figure de style qui se prête très bien aux jeux de mots, elle 
+            est donc fréquemment utilisée pour faire de l'humour. En dehors du côté comique, 
+            elle permet de souligner le second sens du mot en surprenant le lecteur qui doit 
+            s'arrêter un instant pour le saisir après avoir compris le premier.
+            """,
+            Sources = new List<Source>
+            {
+                new()
+                {
+                    DisplayName = "Wikipedia",
+                    Url = new UriBuilder("https://fr.wikipedia.org/wiki/Antanaclase").Uri,
+                },
+                new()
+                {
+                    DisplayName = "Larousse",
+                    Url = new UriBuilder("https://www.larousse.fr/dictionnaires/francais/antanaclase/10911044").Uri,
+                },
+                new()
+                {
+                    DisplayName = "La culture générale",
+                    Url = new UriBuilder("https://www.laculturegenerale.com/antanaclase-diaphore-definition-exemples/").Uri,
+                },
+            },
+            Usages = new List<Usage>
+            {
+                new()
+                {
+                    Example = "Le cœur a ses raisons que la raison ne connaît point.",
+                    Source = "Pascal"
+                },
+                new()
+                {
+                    Example = "Adieu, monsieur l'homme d'affaires, qui n'avez fait celles de personne.",
+                    Source = "Marivaux, Madame Argante I, X, La Mère confidente"
+                },
+                new()
+                {
+                    Example = "Les étudiants, c'est comme le linge, quand il fait beau, ça sèche.",
+                }
+            }
+        }
     }
     .Select(Sanitized);
 
@@ -80,7 +132,7 @@ internal static class ApplicationDbContextExtensions
                 .Replace("\r\n", string.Empty),
 
             Source = raw.Source
-                .Trim()
+                ?.Trim()
                 .Replace("\r\n", string.Empty),
         };
 
