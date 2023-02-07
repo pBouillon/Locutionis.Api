f2c60dc8d00a0a262e0917a62866147107cb9818
pBouillon <pro.pierre.bouillon@protonmail.com>
2023-02-07 23:40:48 +0100

    Add the Pléonasme

--- Locutionis.Api/Persistence/ApplicationDbContextExtensions.cs
+++ Locutionis.Api/Persistence/ApplicationDbContextExtensions.cs
@@ -911,6 +911,45 @@ internal static class ApplicationDbContextExtensions
             },
         },
 
+        // Pléonasme
+        new FigureOfSpeech
+        {
+            Name = "Pléonasme",
+            Description = """
+            Utilisation de plusieurs mots pour dire la même chose, ce qui rend la phrase inutilement 
+            répétitive ou longue. Il ne s'agit pas d'une faute de français mais d'un alourdissement 
+            non nécessaire de la phrase.
+            """,
+            Purpose = """
+            Le pléonasme est souvent utilisé pour renforcer ou souligner un point particulier, mais 
+            peut également être utilisé pour créer un effet humoristique ou de confusion.
+            """,
+            Sources = new List<Source>
+            {
+                new()
+                {
+                    DisplayName = "La culture générale",
+                    Url = new UriBuilder("https://www.laculturegenerale.com/pleonasmes-definition-exemples/").Uri,
+                },
+                new()
+                {
+                    DisplayName = "Larousse",
+                    Url = new UriBuilder("https://www.larousse.fr/dictionnaires/francais/pl%C3%A9onasme/61617").Uri,
+                },
+            },
+            Usages = new List<Usage>
+            {
+                new()
+                {
+                    Example = "Il a fait un mauvais cauchemar",
+                },
+                new()
+                {
+                    Example = "Je vais faire de la marche à pieds ce week-end",
+                },
+            },
+        },
+
         // Prétérition
         new FigureOfSpeech
         {
